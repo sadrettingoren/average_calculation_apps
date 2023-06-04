a4e8e6503add6e6bd191c5ba6b73a3a1e9b0deea
Sadrettin Gören <sadretting64@gmail.com>
2023-06-04 21:21:39 +0300

    add: project added

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:dinamik_not_ortalama_app/constants/app_constants.dart';
+import 'package:dinamik_not_ortalama_app/widgets/ortalama_app.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Ortalama Hesapla',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+          primaryColor: Sabitler.anaRenk,
+          visualDensity: VisualDensity.adaptivePlatformDensity),
+      home: OrtalamaHesapla(),
+    );
+  }
+}
